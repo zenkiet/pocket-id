cf3084cfa8e151c78f55a24af05dddb9d3a0fc71
Elias Schneider <login@eliasschneider.com>
2025-03-30 22:30:22 +0200

    refactor: remove cors exception from middleware as this is handled by the handler

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -15,13 +15,7 @@ func NewCorsMiddleware() *CorsMiddleware {
 
 func (m *CorsMiddleware) Add() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Allow all origins for the token endpoint
-		if c.FullPath() == "/api/oidc/token" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		} else {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", common.EnvConfig.AppURL)
-		}
-
+		c.Writer.Header().Set("Access-Control-Allow-Origin", common.EnvConfig.AppURL)
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
